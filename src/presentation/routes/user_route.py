5e6b2a22024b1b4147b9c6fd4291306123241bbb
wilsonborba <wilsonmatheuslimaborba@gmail.com>
2025-09-14 16:12:50 +0700

    fix(auth): try block to avoid 500 code

--- src/presentation/routes/user_route.py
+++ src/presentation/routes/user_route.py
@@ -4,7 +4,7 @@ from fastapi.params import Depends
 from src.domain.models.user_model import FirebaseUserModel
 from src.presentation.handler.user_handler import get_all_user_info_from_db, sign_up_user, log_in_user
 from ..handler.responses import MyResponse
-
+from src.core.logs import error
 from src.presentation.handler.auth import verify_api_key
 
 
@@ -39,13 +39,20 @@ def post_sign_up_user(raw_user_data: FirebaseUserModel):
     Sign up a new user.
     This endpoint handles the creation of a new user in both the database and Firebase.
     """
+    try:
 
-
-    return MyResponse(
-        status=status.HTTP_201_CREATED,
-        message="User signed up successfully.",
-        data=sign_up_user(raw_user_data)
-    )
+        return MyResponse(
+            status=status.HTTP_201_CREATED,
+            message="User signed up successfully.",
+            data=sign_up_user(raw_user_data)
+        )
+    except Exception as e:
+        error(str(e))
+        return MyResponse(
+            status=status.HTTP_400_BAD_REQUEST,
+            message="User sign-up failed. Please check the provided data.",
+            data=None
+        )
 
 @user_sync_v1.patch(f"/log-in",
              summary="Log In User", tags=["User"],
@@ -57,9 +64,16 @@ def post_log_in_user(raw_user_data: FirebaseUserModel):
     Log in a user.
     This endpoint handles user authentication and returns user data if successful.
     """
-
-    return MyResponse(
-        status=status.HTTP_200_OK,
-        message="User logged in successfully.",
-        data=log_in_user(raw_user_data)
-    )
+    try:
+        return MyResponse(
+            status=status.HTTP_200_OK,
+            message="User logged in successfully.",
+            data=log_in_user(raw_user_data)
+        )
+    except Exception as e:
+        error(str(e))
+        return MyResponse(
+            status=status.HTTP_401_UNAUTHORIZED,
+            message="Authentication failed. Please check your credentials.",
+            data=None
+        )
